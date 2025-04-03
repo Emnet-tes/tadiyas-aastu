684450855e11f79b9de339cdc2df283c46e7a149
Emnet-tes <teshomeemnet255@gmail.com>
2025-04-03 16:42:04 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // Cast 'window' to the correct type
-      const telegramWindow = window as Window & { TelegramWebview?: unknown };
-
-      if (telegramWindow.TelegramWebview) {
-        const url = window.location.href;
-        const chromeUrl = `intent://${url.replace(
-          /^https?:\/\//,
-          ""
-        )}#Intent;scheme=https;end`;
-        window.location.href = chromeUrl;
-      } else {
-        // Redirect normally to the dashboard
-        router.push("/dashboard");
-      }
-    }
-  }, [router]);
-
-  return null; // No UI is needed, as the page just redirects
+  return redirect("/dashboard");
 }

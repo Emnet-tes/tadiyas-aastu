aa2cb139cafed7dccdae3135754a169b4f3ee17f
Emnet-tes <teshomeemnet255@gmail.com>
2025-04-03 15:41:27 +0300

    redirect to broweser

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (typeof window !== "undefined" && (window as any).TelegramWebview) {
+      const url = window.location.href;
+      const chromeUrl = `intent://${url.replace(
+        /^https?:\/\//,
+        ""
+      )}#Intent;scheme=https;end`;
+      window.location.href = chromeUrl;
+    }
+  }, []);
   return redirect("/dashboard");
 }
